0a5d1fad590babd5bb37ca0bdf7faf30d1d50e9a
Alex <a.ofori@rocketmail.com>
2023-06-19 22:17:11 +0100

    Homework Final Commit

---
+++ Printer/src/main/java/Printer.java
@@ -0,0 +1,14 @@
+public class Printer {
+
+    private int sheetsOfPaperLeft;
+    private int numberOfPages;
+    private int numberOfCopies;
+
+    public Printer(int sheetsOfPaperLeft, int numberOfPages, int numberOfCopies){
+        this.sheetsOfPaperLeft = sheetsOfPaperLeft;
+        this.numberOfPages = numberOfPages;
+        this.numberOfCopies = numberOfCopies;
+    }
+
+
+}
---
+++ Printer/src/test/java/PrinterTest.java
@@ -0,0 +1,13 @@
+public class PrinterTest {
+    Printer printer;
+
+    @Before
+    public void before() {
+        int sheetsOfPaperLeft;
+        int numberOfPages;
+        int numberOfCopies;
+        printer = new Printer('sheetsOfPaperLeft', 'numberOfPages', 'numberOfCopies',);
+
+
+    }
+}
